20ccc1c89560850a97a2cd022dcf9ce645fce193
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-18 23:21:27 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ using std::cin;
 
 int main()
 {
+	double length;
+	double width;
+	double height;
+
+	cout << "Calculating the volume of a pralelepiped" << endl << "Enter the initial data:" << endl;
+	
+	cout << "Length (cm) -> ";
+	cin >> length;
+	cout << "Width (cm) -> ";
+	cin >> width;
+	cout << "Height (cm) -> ";
+	cin >> height;
+
+	// Formula: V = a * b * h
+	cout << "Volume: " << (length * width * height) << " cubic centimeters" << endl;
 
 	_getch();
 	return 0;
