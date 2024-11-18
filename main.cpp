d29f47211558f5385e9e4ce205c8541b00b381fb
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-18 16:21:35 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@ using std::cin;
 
 int main()
 {
+	int uah;
+	int kopecks;
+
+	cout << "Hryvnia: ";
+	cin >> uah;
+	cout << "Kopecks: ";
+	cin >> kopecks;
+
+	int all = (uah * 100) + kopecks;
+	int result = all / 100;
+
+	cout << "Result: " << result << " hryvnia " << all % 100 << " kopecks" << endl;
 
 	_getch();
 	return 0;
