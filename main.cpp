e47d0e02282cb7a6cf31d729f14aa1244762781c
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-18 15:46:34 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ using std::cin;
 
 int main()
 {
+	int year;
+	
+	cout << "Enter the year: ";
+	cin >> year;
+
+	int days = 365 + (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+	cout << "The number of days in the year " << year << " is " << days << endl;
 
 	_getch();
 	return 0;
