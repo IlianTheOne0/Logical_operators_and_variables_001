ddcc1e0877e5975858f3b43a01a9b9a166120f82
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-18 23:41:15 +0200

    Completed Task 5

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
 using std::cout;
 using std::endl;
@@ -7,6 +9,15 @@ using std::cin;
 
 int main()
 {
+	int radius;
+
+	cout << "Calculating the volume of a sphere" << endl << "Enter the initial data:" << endl;
+
+	cout << "Sphere radius (m) -> ";
+	cin >> radius;
+	
+	// Formula: (4/3) * pi * R^3
+	cout << "The volume of the sphere is " << (4.0 / 3.0) * M_PI * pow(radius, 3) << " m^3" << endl;
 
 	_getch();
 	return 0;
