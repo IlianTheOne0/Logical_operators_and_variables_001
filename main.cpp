7c061e8865f61c81f0a5b9b5c355c4df1b24f5bb
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-18 23:30:38 +0200

    Completed Task 4

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@ using std::cin;
 
 int main()
 {
+	int scale;
+	double distance;
+
+	cout << "Calculate the distance between settlements" << "Enter the initial data:" << endl;
+
+	cout << "Map scale (number of kilometers in one centimeter) -> ";
+	cin >> scale;
+	cout << "Distance between points representing settlements (cm) -> ";
+	cin >> distance;
+
+	cout << "The distance between the settlements is " << (scale * distance) << " km" << endl;
 
 	_getch();
 	return 0;
